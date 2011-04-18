f6199319f88e1fbc1e20652be0adbc73c337c74b
woxxy <woxxap@gmail.com>
2011-04-18 10:36:45 +0000

    git-svn-id: svn+ssh://foolrulez.org/var/wwwfoolrulezcom/svn/FoOlSlide/trunk@17 b1ff91d0-13a3-4061-8a9e-2d6d114fe058

--- config.php
+++ config.php
@@ -12,22 +12,22 @@
 $db['default']['hostname'] = 'localhost';
 
 // The username of the MySQL user with access to the database
-$db['default']['username'] = 'foolslide';
+$db['default']['username'] = '';
 
 // The password of the MySQL user
-$db['default']['password'] = 'EFeAGjFTLPU6Pa6L';
+$db['default']['password'] = '';
 
 // The name of the Slide database
-$db['default']['database'] = 'foolslide';
+$db['default']['database'] = '';
 
 // The prefix for the tables in the database
 $db['default']['dbprefix'] = '';
 
 // The admin's email (will be overwritten by database)
-$config['admin_email'] = "woxxap@gmail.com";
+$config['admin_email'] = "";
 
 // Site title (will be overwritten via database)
-$config['site_title'] = "FoOlRulez.org";
+$config['site_title'] = "";
 
 // Session encryption: just tap randomly on your keyboard to make a random key
-$config['encryption_key'] = 'wonderwinded';
+$config['encryption_key'] = '';

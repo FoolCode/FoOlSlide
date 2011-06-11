82d3edbedfc5a3e3abc1c0f4272cca85db0e26ec
woxxy <woxxap@gmail.com>
2011-06-11 02:15:59 +0000

    Fix to "next chapter" returning wrong subchapter
    git-svn-id: svn+ssh://foolrulez.org/var/wwwfoolrulezcom/svn/FoOlSlide/trunk@65 b1ff91d0-13a3-4061-8a9e-2d6d114fe058

--- application/models/chapter.php
+++ application/models/chapter.php
@@ -868,12 +868,12 @@ class Chapter extends DataMapper {
 		if ($chapter->result_count() == 0) {
 			// There aren't subchapters for this chapter. Then let's look for the next chapter
 			$chapter = new Chapter();
-			$chapter->where('comic_id', $this->comic->id)->where('volume', $this->volume)->having('chapter > ', $this->chapter)->where('language', $this->language)->order_by('chapter', 'asc')->limit(1)->get();
+			$chapter->where('comic_id', $this->comic->id)->where('volume', $this->volume)->having('chapter > ', $this->chapter)->where('language', $this->language)->order_by('chapter', 'asc')->order_by('subchapter', 'asc')->limit(1)->get();
 			if ($chapter->result_count() == 0) {
 				// Check if there's a chapter in the next volume.
 				// This works even if chapter goes vol2 33 -> vol3 34 or vol2 33 -> vol3 1
 				$chapter = new Chapter();
-				$chapter->where('comic_id', $this->comic->id)->having('volume > ', $this->volume)->where('language', $this->language)->order_by('chapter', 'asc')->limit(1)->get();
+				$chapter->where('comic_id', $this->comic->id)->having('volume > ', $this->volume)->where('language', $this->language)->order_by('chapter', 'asc')->order_by('subchapter', 'asc')->limit(1)->get();
 				if ($chapter->result_count() == 0) {
 					// There's no next chapter. Redirect to the comic page.
 					return site_url('/reader/read/' . $this->comic->stub);

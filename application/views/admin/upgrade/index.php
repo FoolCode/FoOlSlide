10db93fcef108326eb661e0907cb7166da34a43e
Fabio Woxxy <woxxy@foolrulez.org>
2011-07-30 18:34:58 +0200

    Updating Italian

--- application/views/admin/upgrade/index.php
+++ application/views/admin/upgrade/index.php
@@ -24,6 +24,8 @@ echo buttoner();
 echo '<br/><br/>
 	Current version: '.$current_version.'<br/>
 	Latest version available: '.($new_versions?($new_versions[0]->version.'.'.$new_versions[0]->subversion.'.'.$new_versions[0]->subsubversion):_('Your FoOlSlide is up to date.')).'<br/><br/>';
+?>
+<?php echo _('Please, check the <a href="https://www.transifex.net/projects/p/foolslide/resource/defaultpot/">transifex translation project</a> in case you\'re using a non-English language for FoOlSlide. Make sure the % on your language isn\'t under 90% before upgrading. Contribute to the translation to get your language updated in the next localization update of FoOlSlide (normally within 48 hours).');
 if($new_versions){
 	echo '<div class="list">';
 	foreach($new_versions as $version){

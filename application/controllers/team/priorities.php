0435ef01f703715fddf2bcff34ff7ff0fdc407e9
Fabio Woxxy <woxxy@foolrulez.org>
2011-08-29 08:56:51 +0200

    Added basic routing

--- application/controllers/team/priorities.php
+++ application/controllers/team/priorities.php
@@ -8,21 +8,13 @@ class Priorities extends Team_Controller
 	function __construct()
 	{
 		parent::__construct();
-		
-
-		// if this is a load balancer, let's not allow people in the series tab
-		if (get_setting('fs_balancer_master_url'))
-			redirect('/admin/members');
-
-		$this->load->model('files_model');
-		$this->load->library('pagination');
 		$this->viewdata['controller_title'] = _("Series");
 	}
 
 
 	function index()
 	{
-		
+		echo 'This is the priorities system';
 	}
 
 }
--- application/libraries/Team_Controller.php
+++ application/libraries/Team_Controller.php
@@ -16,7 +16,7 @@ class Team_Controller extends MY_Controller
 		}
 		
 		// if the user isn't in any team, he shouldn't be here
-		if (!($teams = $this->tank_auth->is_team()))
+		if (!($this->teamc->teams = $this->tank_auth->is_team()))
 		{
 			flash_notice('notice', 'You can\'t access the team panel because you aren\'t in any team');
 			redirect('account');
@@ -26,6 +26,24 @@ class Team_Controller extends MY_Controller
 
 	public function _remap($method, $params = array())
 	{
+		// index means priorities page. you can be here only when uri is /team/
+		if(($uri_team = $this->uri->segment(2)) === FALSE && $method == "index")
+		{
+			return call_user_func_array(array($this, $method), $params);
+		}
+		
+		// find if any team is set in $method
+		foreach($this->teamc->teams as $key => $team)
+		{
+			if($uri_team == $team->stub)
+			{
+				echo 'success '.$team->stub;
+			}
+			else
+			{
+				echo 'fail'.$uri_team;
+			}
+		}
 		
 	}
 

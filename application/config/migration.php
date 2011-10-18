3cd4bbe892047cff4d816a7fb8c73904a02d30e0
Woxxy <woxxy@foolrulez.org>
2011-10-18 09:54:46 +0200

    Imported the old migration system

--- application/config/migration.php
+++ application/config/migration.php
@@ -9,7 +9,7 @@
 | and disable it back when you're done.
 |
 */
-$config['migrations_enabled'] = TRUE;
+$config['migration_enabled'] = TRUE;
 
 
 /*
@@ -22,7 +22,7 @@ $config['migrations_enabled'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migrations_version'] = 4;
+$config['migration_version'] = 5;
 
 
 /*
@@ -35,7 +35,7 @@ $config['migrations_version'] = 4;
 | Also, writing permission is required within the migrations path.
 |
 */
-$config['migrations_path'] = APPPATH . 'migrations/';
+$config['migration_path'] = APPPATH . 'migrations/';
 
 
 /* End of file migration.php */
--- application/controllers/admin/database.php
+++ application/controllers/admin/database.php
@@ -3,27 +3,33 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 
-class Database extends Admin_Controller {
-
-	function __construct() {
+class Database extends Admin_Controller
+{
+	function __construct()
+	{
 		parent::__construct();
+		// don't redirect if it's coming from the command line
+		// that's because command line has no login skills...
 		$this->tank_auth->is_admin() or redirect('admin');
-		
+
 		// power the migration library
-//		$this->config->load('migrations');
-		$this->load->library('migrations');
-		
+		$this->load->library('migration');
+		$this->config->load('migration');
+
 		// title on top
 		$this->viewdata['controller_title'] = _("Database");
 	}
+
 
 	/*
 	 * Just shows the database admin page. Can be seen by admin only.
 	 * 
 	 * @author Woxxy
 	 */
-	function upgrade() {
-		if (!$this->tank_auth->is_admin()) {
+	function upgrade()
+	{
+		if (!$this->tank_auth->is_admin())
+		{
 			show_404();
 			return FALSE;
 		}
@@ -31,16 +37,16 @@ class Database extends Admin_Controller {
 		// get the migration version
 		$db_version = $this->db->get('migrations')->row()->version;
 		$config_version = $this->config->item('migration_version');
-		
+
 		// if the version is the same, there's no need to be in this page
-		if ($db_version == $config_version) {
+		if ($db_version == $config_version)
+		{
 			redirect('admin');
 		}
 
 		// subtitle on top
 		$this->viewdata['function_title'] = _('Upgrade');
-		$this->viewdata['form_title'] = _('Upgrade');
-			
+
 		// variable for suggesting command via command line
 		$data["CLI_code"] = 'php ' . FCPATH . 'index.php admin database do_upgrade';
 
@@ -49,25 +55,27 @@ class Database extends Admin_Controller {
 		$this->load->view("admin/default.php", $this->viewdata);
 	}
 
+
 	/*
 	 * Does the actual database update, no models used, it's all in controller
 	 * This supports command line for avoiding timeouts in large installations
 	 * 
 	 * @author Woxxy
 	 */
-	function do_upgrade() {
+	function do_upgrade()
+	{
 		if (!isAjax() && !$this->input->is_cli_request())
 			return FALSE;
-
 		// migrate
 		$this->migration->latest();
 
 		// give the correct kind of output, be it JSON via javascript or CLI request
 		if ($this->input->is_cli_request())
-			echo _('Successfully updated the database.') . PHP_EOL;
+			$this->output->set_output(_('Successfully updated the database.') . PHP_EOL);
 		else
 			$this->output->set_output(json_encode(array('href' => site_url('admin/')))); // give the url to go back to
 		return TRUE;
 	}
+
 
 }

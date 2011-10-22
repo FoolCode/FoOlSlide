a21eda48f0b8fecde188f622a45a08a7ab270f96
Woxxy <woxxy@foolrulez.org>
2011-10-22 11:07:39 +0200

    Removed alert in import system, set the hourly admin checks to one hour, changed version to 0.8.11

--- application/views/admin/series/import_compressed_list.php
+++ application/views/admin/series/import_compressed_list.php
@@ -138,7 +138,6 @@
 		})
 		
 		index = jQuery('.input_hidden', box).val();
-alert(jQuery('.input_subchapter', box).val());
 		jQuery.post('<?php echo site_url('/admin/series/import/' . $comic->stub) ?>', {
 			action: 'execute',
 			type: 'single_compressed',

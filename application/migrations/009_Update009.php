6093158b2f6ee48fa060ec5f2d79d09591c2994b
ImperialXT <imperialxt@gmail.com>
2012-10-20 13:26:59 -0700

    Update application/migrations/009_Update009.php

--- application/migrations/009_Update009.php
+++ application/migrations/009_Update009.php
@@ -13,7 +13,7 @@ class Migration_Update009 extends CI_Migration {
 				ALTER TABLE `" . $this->db->dbprefix('comics') . "` ADD `genres` VARCHAR( 256 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL AFTER `description`
 		");
 		$this->db->query("
-				ALTER TABLE `" . $this->db->dbprefix('comics') . "` ADD `publisher` ENUM('Weekly Shounen Jump','Weekly Shounen Magazine','Weekly Shounen Sunday','Bessatsu Shounen Magazine','Ulta Jump','Shounen A','Morning','Beans Ace','Shuukan Shounen Champion','Shounen Gangan','Comic Blade','Young King Ours','Jump SQ','Monthly Shounen Magazine','Champion RED','Dengeki Daioh') NOT NULL AFTER `artist`
+				ALTER TABLE `" . $this->db->dbprefix('comics') . "` ADD `publisher` ENUM('Weekly Shounen Jump','Weekly Shounen Magazine','Weekly Shounen Sunday','Bessatsu Shounen Magazine','Ultra Jump','Shounen A','Morning','Beans Ace','Shuukan Shounen Champion','Shounen Gangan','Comic Blade','Young King Ours','Jump SQ','Monthly Shounen Magazine','Champion RED','Dengeki Daioh') NOT NULL AFTER `artist`
 		");
 	}
 
